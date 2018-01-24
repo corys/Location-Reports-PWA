6e4f579e8edee5e17cf83c0a864fd2dbaf3e72ab
Cory Smith <corys@resdat.com>
2018-01-23 17:24:26 -0900

    Add to homescreen

--- src/main.js
+++ src/main.js
@@ -7,9 +7,13 @@ import store from './store'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'add-to-homescreen/dist/style/addtohomescreen.css'
+require('add-to-homescreen/addtohomescreen')
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
+
+window.addToHomescreen()
 
 /* eslint-disable no-new */
 new Vue({
